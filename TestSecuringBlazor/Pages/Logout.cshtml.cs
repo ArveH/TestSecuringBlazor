9cff8059e6911299db72154f1f23d401a5e8ccb3
Arve Hansen <arveh4@outlook.com>
2020-10-19 19:05:41 +0200

    Add project files.

---
+++ TestSecuringBlazor/Pages/Logout.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace TestSecuringBlazor.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        public async Task<IActionResult> OnGetAsync()
+        {
+            // just to remove compiler warning
+            await Task.CompletedTask;
+
+            return SignOut(
+                new AuthenticationProperties
+                {
+                    RedirectUri = "https://localhost:44388"
+                },
+                OpenIdConnectDefaults.AuthenticationScheme,
+                CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
